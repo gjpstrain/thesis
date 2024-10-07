188fe86279bce2fa54c8e443e1edcaf11374efd6
Gabriel Strain <gabriel.strain@manchester.ac.uk>
2024-10-07 14:38:09 +0100

    more gen methods work

--- helper_functions.R
+++ helper_functions.R
@@ -17,9 +17,10 @@ lapply(chapters_qmd, function(file) trackdown::download_file(file = file,
 
 # code for authenticating trackdown
 
-#my_client <- gargle::gargle_oauth_client(
-#  name = "my-client-name",
-#  id = "client-id",
-#  secret = "secret"
-#)
-#trackdown_auth_configure(client = my_client)
+my_client <- gargle::gargle_oauth_client(
+  name = "my-client-name",
+  id = "628066758971-i11bef3vhj9drrodu6b63l84squh3ev1.apps.googleusercontent.com",
+  secret = "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"
+)
+
+trackdown_auth_configure(client = my_client)
