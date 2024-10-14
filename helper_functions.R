8c679b5a22aebb5e5353a985408c8f483077547f
Gabriel Strain <gabriel.strain@manchester.ac.uk>
2024-10-14 14:40:16 +0100

    eod

--- helper_functions.R
+++ helper_functions.R
@@ -17,10 +17,3 @@ lapply(chapters_qmd, function(file) trackdown::download_file(file = file,
 
 # code for authenticating trackdown
 
-my_client <- gargle::gargle_oauth_client(
-  name = "my-client-name",
-  id = "628066758971-i11bef3vhj9drrodu6b63l84squh3ev1.apps.googleusercontent.com",
-  secret = "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"
-)
-
-trackdown_auth_configure(client = my_client)
