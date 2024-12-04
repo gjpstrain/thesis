c713dac7f7fc43e029b65aed2a2522c3f19f101d
gjpstrain <gabrielstrain@gmail.com>
2024-12-04 11:11:26 +0000

    new reformatting code, aspect ratio issue fixed but not perfect

--- reformat_tex.r
+++ reformat_tex.r
@@ -1,4 +1,3 @@
-
 replace_before_document <- function(file_path) {
   lines <- readLines(file_path)
   preamble_index <- grep("\\\\begin\\{document\\}", lines)
@@ -9,12 +8,53 @@ replace_before_document <- function(file_path) {
     
     new_lines <- grep("\\\\maketitle", new_lines, invert = TRUE, value = TRUE)
     
-    new_lines <- gsub(
-      "(\\\\includegraphics)(\\[(?![^]]*width=\\\\textwidth)[^]]*\\])?(\\{[^\\}]+\\})", 
-      "\\\\includegraphics[width=\\\\textwidth\\2]\\3", 
-      new_lines, 
-      perl = TRUE
-    )
+    new_lines <- sapply(new_lines, function(line) {
+      match <- regexpr("(\\\\includegraphics)(\\[[^\\]]*\\])?(\\{[^\\}]+\\})", line, perl = TRUE)
+      
+      if (match[1] != -1) {
+        graphics_command <- regmatches(line, match)[1]
+        
+        modified_command <- graphics_command
+        
+        # Check if 'width=' is missing
+        if (!grepl("width=", graphics_command)) {
+          if (grepl("\\[[^\\]]*\\]", graphics_command)) {
+            modified_command <- sub(
+              "(\\\\includegraphics\\[)([^\\]]*)",
+              "\\1width=\\\\textwidth,\\2",
+              modified_command
+            )
+          } else {
+            modified_command <- sub(
+              "(\\\\includegraphics)(\\{)",
+              "\\1[width=\\\\textwidth]\\2",
+              modified_command
+            )
+          }
+        }
+        
+        # Check if 'height=' is missing
+        if (!grepl("height=", modified_command)) {
+          if (grepl("\\[[^\\]]*\\]", modified_command)) {
+            modified_command <- sub(
+              "(\\\\includegraphics\\[)([^\\]]*)",
+              "\\1height=\\\\textheight,\\2",
+              modified_command
+            )
+          } else {
+            modified_command <- sub(
+              "(\\\\includegraphics)(\\{)",
+              "\\1[height=\\\\textheight]\\2",
+              modified_command
+            )
+          }
+        }
+        
+        regmatches(line, match) <- modified_command
+      }
+      
+      line
+    }, USE.NAMES = FALSE)
     
     writeLines(new_lines, file_path)
     cat("Replaced preamble, removed \\maketitle, and modified \\includegraphics in", file_path, "\n")
@@ -24,7 +64,9 @@ replace_before_document <- function(file_path) {
 }
 
 
+
 replace_before_document_in_folder <- function(folder_path) {
   files <- list.files(folder_path, pattern = "\\.tex$", full.names = TRUE)
   lapply(files, replace_before_document)
 }
+
