ba9a9c63164aeffa30e12939caad3f6814fbc55f
Gabriel Strain <gabriel.strain@manchester.ac.uk>
2026-02-19 13:58:50 +0000

    more corrections done

--- finalise_thesis.r
+++ finalise_thesis.r
@@ -9,8 +9,8 @@
 
 # First, install required packages
 
-install.packages("quarto")
-install.packages("tinytex")
+#install.packages("quarto")
+#install.packages("tinytex")
 
 # Load packages in
 
@@ -23,7 +23,7 @@ quarto_render("chapters_quarto/", output_format = "latex")
 
 # Following command renders only a single chapter:
 
-quarto_render("chapters_quarto/8_conclusion.qmd", output_format = "latex")
+quarto_render("chapters_quarto/7_belief_change.qmd", output_format = "latex")
 
 # functions to reformat tex are stored in the local directory
 
